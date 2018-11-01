c1b602319015992b2d9bc25bf292e9b092647016
maly phongsavanh and victoria martinez <maly@>
2018-11-01 09:26:40 -0700

    player 1 vs player 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 // business logic
-var player1 = new Players ("Maly");
-var player2 = new Players ("Victoria");
-
 function Players(name, score, dice) {
   this.name = name,
   this.dice = dice,
-  this.totalScore = 0
+  this.totalScore = 0;
+  this.id = 0
 }
 
 function diceRoll() {
@@ -25,21 +23,40 @@ Players.prototype.holding = function() {
 }
 
 
+Players.prototype.assignId = function() {
+
+}
+
+
 // // useriterface logic
 $(document).ready(function() {
+  $(".player-select").submit(function(event) {
+    event.preventDefault();
+    var player1 = new Players ($("#player1").val());
+    var player2 = new Players ($("#player2").val());
 
+    $("#players-display").show();
+    $(".player-select").hide();
+    $("#player1Name").text(player1.name)
+    $("#player2Name").text(player2.name)
 
-  $("#roll-button").click(function() {
+  $("#player1-button").click(function() {
     player1.rolling();
   $("#placeholder").text(player1.dice);
-  console.log(player1);
-
-  })
   $("#hold-button").click(function() {
     player1.holding();
-    $("#player1-game").text(player1.totalScore);
-    console.log(player1);
+    $("#player1Score").text(player1.totalScore);
+    })
+  });
 
+  $("#player2-button").click(function() {
+    player2.rolling();
+  $("#placeholder").text(player2.dice);
+  $("#2hold-button").click(function() {
+    player2.holding();
+  $("#player2Score").text(player2.totalScore);
+
+      })
+    })
   })
-
 });
